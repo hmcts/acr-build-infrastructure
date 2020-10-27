b62c0f887f4c5c68df7c36dcbd341177f25a632d
tomaszcwikcs <tomasz@cwiksolutions.co.uk>
2020-10-27 17:08:24 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# aks-acr-infra
+AKS Azure Container Registry
+
+Contains a pipeline for building public and private container registries with a terraform 
---
+++ terraform/modules/acr/10-container-registry.tf
@@ -0,0 +1,43 @@
+resource "azurerm_resource_group" "acr_resource_group" {
+  location = var.location
+
+  name = format("%s-acr-rg",
+  var.project
+  )
+  tags = local.common_tags
+}
+
+
+#--------------------------------------------------------------
+# Public Azure Container Registry
+#--------------------------------------------------------------
+
+resource "azurerm_container_registry" "container_registry_public" {
+  name                = format("%shmctspublic",
+  var.project
+  )
+  resource_group_name = azurerm_resource_group.acr_resource_group.name
+  location            = var.location
+  admin_enabled       = "true"
+  sku                 = "Premium"
+
+  tags = local.common_tags
+
+}
+
+#--------------------------------------------------------------
+# Private Azure Container Registry
+#--------------------------------------------------------------
+
+resource "azurerm_container_registry" "container_registry_private" {
+  name                = format("%shmctsprivate",
+  var.project
+  )
+  resource_group_name = azurerm_resource_group.acr_resource_group.name
+  location            = var.location
+  admin_enabled       = "true"
+  sku                 = "Premium"
+
+  tags = local.common_tags
+
+}
