c72c0c99e1d21b6c4a3bdd7444655c36df6dc440
Niall King <niall.king@justice.gov.uk>
2025-06-30 14:41:51 +0100

    fix linting error

--- components/cft_acr/10-container-registry.tf
+++ components/cft_acr/10-container-registry.tf
@@ -28,13 +28,19 @@ resource "azurerm_container_registry" "container_registry" {
   sku                      = each.value.sku
   retention_policy_in_days = 1
   tags                     = module.tags.common_tags
+}
 
-  dynamic "geo_replication_locations" {
-    for_each = try(each.value.geo_replication_locations, [])
-    content {
-      location                = geo_replication_locations.value.location
-      zone_redundancy_enabled = try(geo_replication_locations.value.zone_redundancy_enabled, null)
-      tags                    = module.tags.common_tags
-    }
+resource "azurerm_container_registry_replication" "replica" {
+  for_each = {
+    for acr_name, acr in var.cft_acr : 
+    acr_name => acr.geo_replication_locations
+    if contains(keys(acr), "geo_replication_locations")
   }
+
+  name                    = "${each.key}-replica"
+  location                = each.value[0].location
+  resource_group_name     = azurerm_resource_group.cft_acr_resource_group.name
+  registry_name           = azurerm_container_registry.container_registry[each.key].name
+  zone_redundancy_enabled = try(each.value[0].zone_redundancy_enabled, null)
+  tags                    = module.tags.common_tags
 }
