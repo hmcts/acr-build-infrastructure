47ec46f2c70db25bd8f948f53002189569c6fb8b
Niall King <niall.king@justice.gov.uk>
2025-06-30 15:56:16 +0100

    fix linting error

--- components/cft_acr/10-container-registry.tf
+++ components/cft_acr/10-container-registry.tf
@@ -30,8 +30,8 @@ resource "azurerm_container_registry" "container_registry" {
   tags                     = module.tags.common_tags
 
   geo_replication_locations {
-    location                  = "ukwest"
-    zone_redundancy_enabled   = false
+    location                  = each.value.location
+    zone_redundancy_enabled   = each.value.zone_redundancy_enabled
     tags                      = module.tags.common_tags
   }
 }
