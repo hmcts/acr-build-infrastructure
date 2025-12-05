7932826d2bc9f13f918d13a3f1483138599f9554
Daniel Kiriasidis Christou <daniel.christou@justice.gov.uk>
2025-12-05 14:43:53 +0000

    Add AcrPull role assignments for DTS CFT and SDS Developers on zone-redundant ACRs

--- components/zr_acr/10-container-registry.tf
+++ components/zr_acr/10-container-registry.tf
@@ -20,3 +20,31 @@ resource "azurerm_container_registry" "container_registry" {
   retention_policy_in_days = 1
   tags                     = module.tags.common_tags
 }
+
+#--------------------------------------------------------------
+# ACRPull Role Assignments for Developer Groups
+#--------------------------------------------------------------
+
+data "azuread_group" "cft_developers" {
+  display_name = "DTS CFT Developers"
+}
+
+data "azuread_group" "sds_developers" {
+  display_name = "DTS SDS Developers"
+}
+
+resource "azurerm_role_assignment" "cft_developers_acr_pull" {
+  for_each = var.zr_acr
+
+  scope                = azurerm_container_registry.container_registry[each.key].id
+  role_definition_name = "AcrPull"
+  principal_id         = data.azuread_group.cft_developers.object_id
+}
+
+resource "azurerm_role_assignment" "sds_developers_acr_pull" {
+  for_each = var.zr_acr
+
+  scope                = azurerm_container_registry.container_registry[each.key].id
+  role_definition_name = "AcrPull"
+  principal_id         = data.azuread_group.sds_developers.object_id
+}
